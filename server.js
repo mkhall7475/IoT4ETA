00af5103fa3624ddc33878bf52ba7f8e11611441
mkhall7475 <kimhall@worldtweak.com>
2017-07-29 14:36:56 -0500

    KimsTraffic
    commented out the password and login - not needed to deploy to Azure

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
 //////// Customize this data! /////////
 //
-var startpoint = "Microsoft, 10900 Stonelake Blvd. Austin, TX 78759"; // Commute starting address
-var endpoint = "Pinballz, 8940 Research Blvd Ste 100, Austin, TX 78758"; // Commute ending address
-var trafficlowthreshold = 3; // If traffic adds 3 min. or less to commute, traffic is "low."
-var traffichighthreshold = 10; // If traffic adds 10 min. or more to commute, traffic is "high." If traffic adds between 3 and 10 minutes to commute, traffic is "medium."
+var startpoint = "Home, 7475 Brentcove Circle, Dallas, TX 75214"; // Commute starting address
+var endpoint = "Dallas County Records Building, 509 Main St Ste 200, Dallas, TX 75202"; // Commute ending address
+var trafficlowthreshold = 1; // If traffic adds 3 min. or less to commute, traffic is "low."
+var traffichighthreshold = 3; // If traffic adds 10 min. or more to commute, traffic is "high." If traffic adds between 3 and 10 minutes to commute, traffic is "medium."
 var refreshrate = 5; // Traffic data is updated every 5 minutes.
-var myparticleemail = "StoreThisLocallyOrInAzure" // The email address you use to log in to build.particle.io.
-var myparticlepw = "StoreThisLocallyOrInAzure" // The password you use to log in to build.particle.io.
-var myparticletoken = "StoreThisLocallyOrInAzure" // Photon token (build.particle.io > Settings)
-var mybingmapskey = "StoreThisLocallyOrInAzure" // Bing Maps API Key (bingmapsportal.com)
-//
+//var myparticleemail = "kimhall@worldtweak.com" // The email address you use to log in to build.particle.io. if you have token, you don't need this.
+//var myparticlepw = "7521s1st3r!PE" // The password you use to log in to build.particle.io. see git hub for information regardin how to deploy to Azure. 
+var myparticletoken = "d7c1b339d7daae4d27dc449f378ee88841d11974" // Photon token (build.particle.io > Settings)if you deploy to Azure process.env.nameOfVariable.
+var mybingmapskey = "Aj0HkOdIWyiuN9aYMWPw~7BdfH2cngVT30ybDW5En9Q~AnTjOhp0JoPpMQ5oM07vYsRNvICqaPrmmMCS3kGY7ifYjn_oRddQMZFtUI9ewjCo" // Bing Maps API Key (bingmapsportal.com)
+//https://github.com/hxlnt/iot4eta//
 ///////////////////////////////////////
 
 // Set up Photon
 
 var Particle = require("particle-api-js");
 var particle = new Particle();
-particle.login({ username: myparticleemail, password: myparticlepw });
+//particle.login({ username: myparticleemail, password: myparticlepw });
 
 // Set up Bing Maps API and traffic data
 
